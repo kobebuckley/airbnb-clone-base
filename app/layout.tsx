307602816f2344dd7c438358d674bc0f8d14d037
Kobe <kobetbuckley@gmail.com>
2023-05-22 12:02:42 -1000

    53:35

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import {Nunito} from "next/font/google"
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Navbar from "./components/navbar/Navbar"
+import ClientOnly from './components/ClientOnly'
+import Modal from "./components/modals/Modal"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -23,8 +25,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        
-        <Navbar />
+        <ClientOnly>
+          <Modal title="Hello World" isOpen/>
+          <Navbar />
+        </ClientOnly>
         {children}
         </body>
     </html>
