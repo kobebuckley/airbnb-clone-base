2fce57b7fc59dad7a110ec50e08457a6e23dfb14
Kobe <kobetbuckley@gmail.com>
2023-05-22 13:26:40 -1000

    https://youtu.be/c_-b_isI4vg?t=3970

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,8 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import Navbar from "./components/navbar/Navbar"
 import ClientOnly from './components/ClientOnly'
-import Modal from "./components/modals/Modal"
+
+import RegisterModal from "./hooks/RegisterModal"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -26,7 +27,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-          <Modal actionLabel="Submit" title="Hello World" isOpen/>
+          <RegisterModal />
           <Navbar />
         </ClientOnly>
         {children}
